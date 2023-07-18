eefce943214f5d1a4ee22a80f94c09689cbf106f
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-18 11:44:57 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure
+{
+    public struct Student
+    {
+        int id;
+        string name;
+        public Student(int id,string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        public void display()
+        {
+            Console.WriteLine($"Student Id : {id} \n Student Name : {name};");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud = new Student(23, "Raj");
+            stud.display();
+            Student stud1 = new Student(12, "Ravi");
+            stud1.display();
+        }
+    }
+}
